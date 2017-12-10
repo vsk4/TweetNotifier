d90a8b19e4b5b6f457cede71697040dd50d08bf6
SAI KUMAR <saikumarvadla4@gmail.com>
2017-12-10 08:31:23 +0530

    Update background.js

--- background.js
+++ background.js
@@ -9,13 +9,13 @@ $(function(){
 });  
 function engine(){
 		var newtweets=[];
-		$.get('https://twitter.com/i/notifications',function(data){
+		$.get('https://twitter.com/i/notifications',function(data){ //getting data from "https://twitter.com/i/notifications"
 		var htmldata=data;
 		$data= $(htmldata).find('#stream-items-id').eq(0);
 		$data.find('.ActivityItem-quoteTweetContainer').remove();
 		$data.find('.QuoteTweet').remove();
 		$data.find('.ActivityItem-facepileContainer').remove();
-		//$data.find('.activity-timestamp').remove();
+		//$data.find('.activity-timestamp').remove();			//filtering the unwanted data
 		$data.find('.ActivityItem-dismissMenu').remove();
 		$data.find('.ActivityItem-icon').remove();
 		//$data.find('.QuoteTweet').remove();
@@ -23,8 +23,8 @@ function engine(){
 
 
 
-		$('body').append($data);
-			for(i=0;i<$data.find('li.stream-item').length;i++)
+		$('body').append($data);  // appending the data to th html body
+			for(i=0;i<$data.find('li.stream-item').length;i++)  // filtering every li item.
 			{
 			ids[i]=$data.find('li.stream-item').eq(i).attr('data-item-id');
 	                messages[i]= ($($data).find('li.stream-item').eq(i).find('div.ActivityItem-displayText').text()).replace(/\n/g,'      ').trim();
@@ -42,7 +42,7 @@ function engine(){
                           iconUrl :"git.png",
                              };
 
-                               	chrome.notifications.create(first);
+                               	chrome.notifications.create(first);  //google api for notifying the first popup
                              	latestid=ids[0];
 
 			}
@@ -78,18 +78,19 @@ function engine(){
                     			      message: newtweets[i],
                       			      contextMessage:"Rich Notifier",
                     			      buttons:[{
-                       				   	title:"open link"
+                       				   	title:"open link"      //link in the notification panel
                         			    }],
                        			      iconUrl :"icon.png",
                       			    };
-                          		       chrome.notifications.onButtonClicked.addListener(function(){
-                          			window.open('https://twitter.com/i/notifications');
+                          		       chrome.notifications.onButtonClicked.addListener(function(){  // google api for  notifying the notifications
+                          			window.open('https://twitter.com/i/notifications');   //function to open the link on clicking the button
                           			});
                                   	 chrome.notifications.create(tweet);
 
 					}
 				     }
-						console.log(newtweets);
+						console.log(newtweets);  // shows new tweets on the console
+			
 						console.log(latestid);
 	});
 }
